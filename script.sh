e58c5105419aa4e18a05280859f4be485fee1aec
Ivan Lovkin <terra144481@gmail.com>
2023-07-19 17:06:01 +0200

    change script
    Signed-off-by: Ivan Lovkin <terra144481@gmail.com>

--- script.sh
+++ script.sh
@@ -8,4 +8,4 @@ sudo touch  ~thermo/.ssh/authorized_keys
 sudo chmod 600 ~thermo/.ssh/authorized_keys
 sudo chmod 700 ~thermo/.ssh/
 sudo chown -R thermo:thermo  /home/thermo/.ssh
-sudo usermod -aG docker thermo
+
