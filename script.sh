fb54dcc121ef4ae94ff16bd40ab6c16d1adc3784
Ivan Lovkin <terra144481@gmail.com>
2023-07-19 16:27:25 +0200

    add gitignore file+edit script
    Signed-off-by: Ivan Lovkin <terra144481@gmail.com>

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 apt-get update
 ssh -V
-sudo adduser thermo &&
-sudo usermod -aG sudo thermo &&
+sudo adduser thermo
+sudo usermod -aG sudo thermo
 sudo mkdir ~thermo/.ssh
 sudo touch  ~thermo/.ssh/authorized_keys
 sudo chmod 600 ~thermo/.ssh/authorized_keys
